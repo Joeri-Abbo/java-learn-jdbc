642f986ad3fe6c6d89b441f3e26545a25aa2fb92
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-23 22:28:51 +0200

    ICTtrainingen : Different ways to connect to a database.

--- src/main/java/com/skillsoft/jdbc/JDBCIntro.java
+++ src/main/java/com/skillsoft/jdbc/JDBCIntro.java
@@ -3,18 +3,21 @@ package com.skillsoft.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JDBCIntro {
     public static void main(String[] args) throws ClassNotFoundException {
         String databaseURL = "jdbc:mysql://localhost:3306";
 
-        String user = "mysql";
-        String password = "mysql";
+        Properties connectionProps = new Properties();
+
+        connectionProps.put("user", "mysql");
+        connectionProps.put("password", "mysql");
+
         Connection con = null;
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(databaseURL, user, password);
+            con = DriverManager.getConnection(databaseURL, connectionProps);
 
             if (con != null) {
                 System.out.println("The connection has been successfully established!");
