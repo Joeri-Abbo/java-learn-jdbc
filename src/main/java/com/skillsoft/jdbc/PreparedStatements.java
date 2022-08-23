51bfae3a4c2e320b7baae1b37fc702c19a12a2d9
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-24 00:10:35 +0200

    ICTtrainingen : Clean up.

--- src/main/java/com/skillsoft/jdbc/PreparedStatements.java
+++ src/main/java/com/skillsoft/jdbc/PreparedStatements.java
@@ -18,30 +18,10 @@ public class PreparedStatements {
 
         try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
 
-            Statement stmt = con.createStatement();
-//
-//            query = "INSERT INTO employee_data(emp_id,emp_name,designation,salary) values (?,?,?,?)";
-//
-//            PreparedStatement ps = con.prepareStatement(query);
-//
-//            ps.setInt(1, 103);
-//            ps.setString(2, "Raj");
-//            ps.setString(3, "Tech Lead");
-//            ps.setDouble(4, 110000);
-//
-//            ps.executeUpdate();
-//            ps.setInt(1, 104);
-//            ps.setString(2, "Maria");
-//            ps.setString(3, "Product Manager");
-//            ps.setDouble(4, 118000);
-//
-//            ps.executeUpdate();
-
-
             query = "SELECT  emp_id, emp_name, designation FROM employee_data WHERE emp_id = ?";
             PreparedStatement ps = con.prepareStatement(query);
 
-            ps.setInt(1,103);
+            ps.setInt(1, 103);
             System.out.println("Contents of the table");
 
             ResultSet rs = ps.executeQuery();
