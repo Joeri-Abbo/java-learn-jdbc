1cb568735d9efbf105ded3e51fad525bca913c7d
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-24 00:10:11 +0200

    ICTtrainingen : Using Prepared statements.

--- src/main/java/com/skillsoft/jdbc/PreparedStatements.java
+++ src/main/java/com/skillsoft/jdbc/PreparedStatements.java
@@ -1,6 +1,11 @@
 package com.skillsoft.jdbc;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class PreparedStatements {
     public static String dbURL = "jdbc:mysql://localhost:3306/SampleDB";
@@ -14,15 +19,32 @@ public class PreparedStatements {
         try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
 
             Statement stmt = con.createStatement();
+//
+//            query = "INSERT INTO employee_data(emp_id,emp_name,designation,salary) values (?,?,?,?)";
+//
+//            PreparedStatement ps = con.prepareStatement(query);
+//
+//            ps.setInt(1, 103);
+//            ps.setString(2, "Raj");
+//            ps.setString(3, "Tech Lead");
+//            ps.setDouble(4, 110000);
+//
+//            ps.executeUpdate();
+//            ps.setInt(1, 104);
+//            ps.setString(2, "Maria");
+//            ps.setString(3, "Product Manager");
+//            ps.setDouble(4, 118000);
+//
+//            ps.executeUpdate();
 
-            query = "INSERT INTO employee_data(emp_id,emp_name,designation,salary) " + "values (101,'Alan', 'Java Developer', 87000)," + " (102,'Claudia', 'Senior Software Engineer', 102000)";
-            int updatedRows = stmt.executeUpdate(query);
-            System.out.println("Query executed successfully! Rows updated: " + updatedRows);
 
+            query = "SELECT  emp_id, emp_name, designation FROM employee_data WHERE emp_id = ?";
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setInt(1,103);
             System.out.println("Contents of the table");
 
-            query = "SELECT * FROM employee_data order by emp_name";
-            ResultSet rs = stmt.executeQuery(query);
+            ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
                 System.out.print(rs.getString("emp_name") + "\t" + rs.getString("designation") + "\n");
