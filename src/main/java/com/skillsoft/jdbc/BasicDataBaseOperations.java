ec80b68c91070ab80485f005ba3fd877e3cef70e
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-23 23:31:32 +0200

    ICTtrainingen : Running Update and Delete Queries.

--- src/main/java/com/skillsoft/jdbc/BasicDataBaseOperations.java
+++ src/main/java/com/skillsoft/jdbc/BasicDataBaseOperations.java
@@ -20,10 +20,21 @@ public class BasicDataBaseOperations {
 
             Statement stmt = con.createStatement();
 
-            query = "INSERT INTO users (first_name, last_name, email) VALUES ('Julio', 'Chavez', 'chavezj317@gmail.com'), ('Zack', 'Harken', 'Zack_Harken@hotmail.com')";
+            query = "DELETE FROM users WHERE phone_number IS NULL ";
             int result = stmt.executeUpdate(query);
 
-            System.out.println("Query executed successfully! Returned value is: " + result);
+            System.out.println("Query executed successfully! Number of deleted rows: " + result);
+
+
+            query = "SELECT * FROM users";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()){
+                System.out.print(rs.getString("first_name"));
+                System.out.print("\t"+rs.getString("last_name"));
+                System.out.println();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
