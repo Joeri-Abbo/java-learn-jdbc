c38227af87ceab1e42121f6da81383e3d7a82171
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-23 22:40:00 +0200

    ICTtrainingen : Loading Connection Details from a Properties File.

--- src/main/java/com/skillsoft/jdbc/JDBCIntro.java
+++ src/main/java/com/skillsoft/jdbc/JDBCIntro.java
@@ -1,23 +1,14 @@
 package com.skillsoft.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class JDBCIntro {
     public static void main(String[] args) throws ClassNotFoundException {
-        String databaseURL = "jdbc:mysql://localhost:3306";
-
-        Properties connectionProps = new Properties();
-
-        connectionProps.put("user", "mysql");
-        connectionProps.put("password", "mysql");
-
         Connection con = null;
 
         try {
-            con = DriverManager.getConnection(databaseURL, connectionProps);
+            con = MySQLDBUtil.getConnection();
 
             if (con != null) {
                 System.out.println("The connection has been successfully established!");
