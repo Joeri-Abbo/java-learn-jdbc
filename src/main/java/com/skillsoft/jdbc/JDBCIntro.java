1c0dce7bef11411e1d3b59e2c5bc926cd0e98902
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-23 22:47:54 +0200

    ICTtrainingen : Connection to a Database with a DataSource.

--- src/main/java/com/skillsoft/jdbc/JDBCIntro.java
+++ src/main/java/com/skillsoft/jdbc/JDBCIntro.java
@@ -2,13 +2,30 @@ package com.skillsoft.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class JDBCIntro {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Connection con = null;
+
+    public static DataSource getMysqlDataSource() {
+        MysqlDataSource mysqlDS = null;
 
         try {
-            con = MySQLDBUtil.getConnection();
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setUrl("jdbc:mysql://localhost:3306");
+            mysqlDS.setUser("mysql");
+            mysqlDS.setPassword("mysql");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mysqlDS;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        try (Connection con = JDBCIntro.getMysqlDataSource().getConnection()) {
 
             if (con != null) {
                 System.out.println("The connection has been successfully established!");
