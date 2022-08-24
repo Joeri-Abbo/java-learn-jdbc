84c406b619e9f9d76bbc45620da5011fd95666ae
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-24 20:35:51 +0200

    ICTtrainingen : Navigating over a ResultSet.

--- src/main/java/com/skillsoft/jdbc/ResultSetFeatures.java
+++ src/main/java/com/skillsoft/jdbc/ResultSetFeatures.java
@@ -3,25 +3,58 @@ package com.skillsoft.jdbc;
 import java.sql.*;
 
 public class ResultSetFeatures {
-    public static String dbURL = "jdbc:mysql://localhost:3306/";
+    public static String dbURL = "jdbc:mysql://localhost:3306/SampleDB";
     public static String username = "mysql";
     public static String password = "mysql";
 
+
+    private static void displayEmpData(String label, ResultSet result) throws SQLException {
+        int id = result.getInt("emp_id");
+        String name = result.getString("emp_name");
+        String designation = result.getString("designation");
+        Double salary = result.getDouble("salary");
+
+        String empData = "%s: %d | %s | %s | %.2f \n";
+        System.out.format(empData, label, id, name, designation, salary);
+    }
+
     public static void main(String[] args) throws SQLException {
 
+        String query = null;
 
         try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
-            DatabaseMetaData metaData = con.getMetaData();
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-            System.out.println("-- ResultSet Properties --\n");
-            System.out.println("Support TYPE_FORWARD_ONLY:" + metaData.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY));
-            System.out.println("Support TYPE_SCROLL_INSENSITIVE:" + metaData.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE));
-            System.out.println("Support TYPE_SCROLL_SENSITIVE:" + metaData.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE));
+            query = "SELECT * FROM employee_data";
 
-            System.out.println("\n\n-- ResultSet Concurrency --\n");
-            System.out.println("Support CONCUR_READ_ONLY:" + metaData.supportsResultSetType(ResultSet.CONCUR_READ_ONLY));
-            System.out.println("Support CONCUR_UPDATABLE:" + metaData.supportsResultSetType(ResultSet.CONCUR_UPDATABLE));
+            ResultSet rs = stmt.executeQuery(query);
 
+            System.out.println();
+
+            rs.first();
+            displayEmpData("First()", rs);
+
+            System.out.println();
+            rs.relative(3);
+            displayEmpData("Relative(3)", rs);
+
+            System.out.println();
+            rs.previous();
+            displayEmpData("previous()", rs);
+
+            System.out.println();
+            rs.absolute(6);
+            displayEmpData("absolute(6)", rs);
+
+            System.out.println();
+
+            rs.last();
+            displayEmpData("Last()", rs);
+
+            System.out.println();
+            rs.relative(-5);
+            displayEmpData("Relative(-5)", rs);
+            System.out.println();
 
         } catch (SQLException e) {
             e.printStackTrace();
