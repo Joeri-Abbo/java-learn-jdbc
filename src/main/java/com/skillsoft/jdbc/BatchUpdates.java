c4da93859acba8771ef2d9d9fe78b4cb51c54ecf
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-24 22:32:12 +0200

    ICTtrainingen : Prepared Statements and batch operations.

--- src/main/java/com/skillsoft/jdbc/BatchUpdates.java
+++ src/main/java/com/skillsoft/jdbc/BatchUpdates.java
@@ -11,25 +11,54 @@ public class BatchUpdates {
     public static String password = "mysql";
 
     public static void main(String[] args) throws SQLException {
-
-        Statement stmt = null;
-
         try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
 
-            stmt = con.createStatement();
+            con.setAutoCommit(false);
+            PreparedStatement pstmt = con.prepareStatement("INSERT INTO Products values(?,?,?)");
 
-            stmt.addBatch("INSERT INTO Products values(101, 'Mother Board', 79)");
-            stmt.addBatch("INSERT INTO Products values(102, 'Mouse', 15)");
-            stmt.addBatch("INSERT INTO Products values(103, 'HDMI Cable', 5)");
-            stmt.addBatch("INSERT INTO Products values(104, 'Keyboard', 12)");
-            stmt.addBatch("INSERT INTO Products values(105, 'USB cable', 3)");
-            stmt.addBatch("INSERT INTO Products values(106, 'VGA Cable', 3)");
+            pstmt.setInt(1, 101);
+            pstmt.setString(2, "Mother Board");
+            pstmt.setDouble(3, 79);
+            pstmt.addBatch();
 
-            int[] updateCounts = stmt.executeBatch();
+            pstmt.setInt(1, 102);
+            pstmt.setString(2, "Mouse");
+            pstmt.setDouble(3, 15);
+            pstmt.addBatch();
+
+            pstmt.setInt(1, 103);
+            pstmt.setString(2, "HDMI Cable");
+            pstmt.setDouble(3, 5);
+            pstmt.addBatch();
+
+            pstmt.setInt(1, 104);
+            pstmt.setString(2, "Keyboard");
+            pstmt.setDouble(3, 12);
+            pstmt.addBatch();
+
+            pstmt.setInt(1, 105);
+            pstmt.setString(2, "USB cable");
+            pstmt.setDouble(3, 3);
+            pstmt.addBatch();
+
+            pstmt.setInt(1, 106);
+            pstmt.setString(2, "VGA cable");
+            pstmt.setDouble(3, 3);
+            pstmt.addBatch();
+
+            int[] updateCounts = pstmt.executeBatch();
 
             System.out.println(Arrays.toString(updateCounts));
+
+            con.commit();
         } catch (BatchUpdateException e) {
-            e.printStackTrace();
+            System.out.println("----BatchUpdateExecution----");
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("Message: " + e.getMessage());
+            System.out.println("Vendor: " + e.getErrorCode());
+
+            System.out.println(Arrays.toString(e.getUpdateCounts()));
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
