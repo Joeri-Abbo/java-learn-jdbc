033f5481149a714dd20386349eabcead08f0fcbf
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-24 22:53:45 +0200

    ICTtrainingen : Transactions using JDBC.

--- src/main/java/com/skillsoft/jdbc/DBTransactions.java
+++ src/main/java/com/skillsoft/jdbc/DBTransactions.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.*;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public class DBTransactions {
     public static String dbURL = "jdbc:mysql://localhost:3306/SampleDB";
@@ -15,23 +14,42 @@ public class DBTransactions {
         String query = null;
         Connection con = null;
         Statement stmt = null;
+        Savepoint savepoint = null;
 
         try {
             con = DriverManager.getConnection(dbURL, username, password);
+            con.setAutoCommit(false);
 
             stmt = con.createStatement();
 
-            query = "INSERT INTO Products values(103, 'HDMI CABLE',5)";
+            query = "INSERT INTO Products values(104, 'Keyboard',12)";
             stmt.executeUpdate(query);
 
-            query = "INSERT INTO Products values(102, 'Mouse',15)";
+            savepoint = con.setSavepoint("OneRow");
+            query = "INSERT INTO Products values(105, 'USB CABLE',3)";
+            stmt.executeUpdate(query);
+
+            query = "INSERT INTO Products values(106, 'VGA CABLE',3)";
+            stmt.executeUpdate(query);
+
+            query = "INSERT INTO Products values(103, 'LCD Monitor',159)";
             stmt.executeUpdate(query);
 
             System.out.println("Rows successfully added!");
-
+            con.commit();
 
         } catch (SQLException e) {
             e.printStackTrace();
+
+            if (savepoint != null) {
+                System.out.println("Errors detected. Rolling back to savepoint...");
+                con.rollback(savepoint);
+                con.commit();
+
+            } else {
+                System.out.println("Errors detected. Rolling back...");
+                con.rollback();
+            }
         } finally {
             con.close();
         }
