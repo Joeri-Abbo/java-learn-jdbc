4c7c28729a4e19b4dd9f3500057a9ddb7be46f91
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-28 22:45:16 +0200

    ICTtrainingen : The joinRowSet.

---
+++ src/main/java/com/skillsoft/jdbc/JoinRowSetExamples.java
@@ -0,0 +1,49 @@
+package com.skillsoft.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.sql.rowset.RowSetProvider;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.JoinRowSet;
+
+public class JoinRowSetExamples {
+
+    public static String dbURL = "jdbc:mysql://localhost:3306/SampleDB";
+    public static String username = "mysql";
+    public static String password = "mysql";
+
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
+
+            CachedRowSet cachedRsProd = RowSetProvider.newFactory().createCachedRowSet();
+            CachedRowSet cachedRsProdBrand = RowSetProvider.newFactory().createCachedRowSet();
+
+            cachedRsProd.setCommand("SELECT * FROM Products");
+            cachedRsProdBrand.setCommand("SELECT * FROM ProductBrand");
+
+            cachedRsProd.execute(con);
+            cachedRsProdBrand.execute(con);
+
+            JoinRowSet joinRs = RowSetProvider.newFactory().createJoinRowSet();
+
+            joinRs.addRowSet(cachedRsProd, "product_id");
+            joinRs.addRowSet(cachedRsProdBrand, "product_id");
+
+            System.out.println("------Products in stock------");
+
+            while (joinRs.next()) {
+                System.out.print("Brand name: " + joinRs.getString("brand_name") + "\t");
+                System.out.print("Product name: " + joinRs.getString("product_name") + "\t");
+                System.out.print("Price: " + joinRs.getDouble("price") + "\n");
+            }
+
+            joinRs.close();
+            cachedRsProd.close();
+            cachedRsProdBrand.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
