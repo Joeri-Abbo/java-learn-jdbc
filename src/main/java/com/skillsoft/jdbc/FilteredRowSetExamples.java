2398257698a725070db79e5b9ff95650cf3dfed3
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-28 23:38:27 +0200

    ICTtrainingen : Updating a FilteredRowSet.

--- src/main/java/com/skillsoft/jdbc/FilteredRowSetExamples.java
+++ src/main/java/com/skillsoft/jdbc/FilteredRowSetExamples.java
@@ -2,6 +2,8 @@ package com.skillsoft.jdbc;
 
 import javax.sql.rowset.FilteredRowSet;
 import javax.sql.rowset.RowSetProvider;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class FilteredRowSetExamples {
     public static String dbURL = "jdbc:mysql://localhost:3306/SampleDB";
@@ -10,28 +12,27 @@ public class FilteredRowSetExamples {
 
     public static void main(String[] args) {
         FilteredRowSet filteredRs = null;
-        try {
-
+        try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
+            con.setAutoCommit(false);
             filteredRs = RowSetProvider.newFactory().createFilteredRowSet();
 
-            filteredRs.setUsername(username);
-            filteredRs.setPassword(password);
-            filteredRs.setUrl(dbURL);
 
             filteredRs.setCommand("SELECT * FROM Products");
-            filteredRs.execute();
-            System.out.println("--- Unfiltered RowSet ---");
-
-            while (filteredRs.next()) {
-                System.out.print("ID: " + filteredRs.getInt("product_id") + "\t");
-                System.out.print("Product name: " + filteredRs.getString("product_name") + "\t");
-                System.out.print("Price: " + filteredRs.getDouble("price") + "\n");
-            }
-
+            filteredRs.execute(con);
             ProductPriceFilter ppFilter = new ProductPriceFilter(5.0, 15.0, 3);
-
-            filteredRs.beforeFirst();
             filteredRs.setFilter(ppFilter);
+
+            filteredRs.moveToInsertRow();
+
+            filteredRs.updateInt("product_id", 108);
+            filteredRs.updateString("product_name", "Curved LCD Monitor");
+            filteredRs.updateDouble("price", 199);
+
+            filteredRs.insertRow();
+            filteredRs.moveToCurrentRow();
+
+            filteredRs.acceptChanges();
+            filteredRs.beforeFirst();
 
             System.out.println("--- Filtered RowSet ---");
             while (filteredRs.next()) {
@@ -40,7 +41,7 @@ public class FilteredRowSetExamples {
                 System.out.print("Price: " + filteredRs.getDouble("price") + "\n");
             }
 
-
+            filteredRs.commit();
             filteredRs.close();
         } catch (Exception e) {
             e.printStackTrace();
