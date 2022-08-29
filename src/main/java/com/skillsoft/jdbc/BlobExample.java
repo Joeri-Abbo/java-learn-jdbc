d04e44b01faff019d9dc216488d8e676d65753b2
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-29 22:49:34 +0200

    ICTtrainingen : Retrieving BLOBs from a Database.

--- src/main/java/com/skillsoft/jdbc/BlobExample.java
+++ src/main/java/com/skillsoft/jdbc/BlobExample.java
@@ -1,11 +1,13 @@
 package com.skillsoft.jdbc;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.sql.rowset.RowSetProvider;
+import javax.sql.rowset.CachedRowSet;
 
 public class BlobExample {
     public static String dbURL = "jdbc:mysql://localhost:3306/SampleDB";
@@ -13,20 +15,24 @@ public class BlobExample {
     public static String password = "mysql";
 
     public static void main(String[] args) throws FileNotFoundException {
-        PreparedStatement pstmt = null;
+        FileOutputStream outputFile = null;
         try (Connection con = DriverManager.getConnection(dbURL, username, password)) {
 
-            String query = "INSERT INTO PuppyInfo (puppy_id, puppy_breed,puppy_photo) values(?,?,?)";
-            pstmt = con.prepareStatement(query);
+            CachedRowSet cachedRs = RowSetProvider.newFactory().createCachedRowSet();
 
-            pstmt.setInt(1,100);
-            pstmt.setString(2,"American Eskimo");
-            FileInputStream input = new FileInputStream("/Users/joeriabbo/Sites/java/learn-jdbc/pets/american_eskimo.jpeg");
+            cachedRs.setCommand("SELECT * FROM PuppyInfo");
+            cachedRs.execute(con);
+            cachedRs.beforeFirst();
 
-            pstmt.setBlob(3, input);
-            pstmt.executeUpdate();
-            System.out.println("The image upload was a success!");
-        } catch (SQLException e) {
+            while (cachedRs.next()) {
+                byte byteArray[] = cachedRs.getBytes("puppy_photo");
+
+                outputFile = new FileOutputStream("pets/"+cachedRs.getInt("puppy_id") + ".jpg");
+                outputFile.write(byteArray);
+
+                System.out.println("Image of " + cachedRs.getString("puppy_breed") + " saved to pets/" + cachedRs.getInt("puppy_id") + ".jpg");
+            }
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
     }
